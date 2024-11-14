465c25751ab1a27a8d18e68acc3fe611a104a815
uNyanda <ayabulelamdlalose@outlook.com>
2024-11-14 19:38:00 +0200

    changed the display for the weather

---
+++ src/status-left-color.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Get the current directory
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Source the theme colors
+source "$CURRENT_DIR/themes.sh"
+
+status_left_color() {
+  if [[ "$(tmux show-option -gpv client_prefix)" == "1" ]]; then
+    CURRENT_STATUS_LEFT_COLOR="${THEME[lavender]}"
+  else
+    CURRENT_STATUS_LEFT_COLOR="${THEME[surface2]}"
+  fi
+}
+
+CURRENT_STATUS_LEFT_COLOR="${THEME[surface2]}"

a030220e3b3cda631044cedb8876318b70da83e9
uNyanda <ayabulelamdlalose@outlook.com>
2024-11-17 07:41:55 +0200

    bg colors and fg colors

--- src/music-tmux-statusbar.sh
+++ src/music-tmux-statusbar.sh
@@ -14,11 +14,11 @@ fi
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $CURRENT_DIR/themes.sh
 
-ACCENT_COLOR="${THEME[blue]}"
-SECONDARY_COLOR="${THEME[surface1]}"
-BG_COLOR="${THEME[surface1]}"
-BG_BAR="${THEME[surface1]}"
-TIME_COLOR="${THEME[crust]}"
+ACCENT_COLOR="#[fg=${THEME[blue]},bg=default]"
+SECONDARY_COLOR="#[fg=${THEME[surface2]},bg=default]"
+BG_COLOR="#[fg=${THEME[surface2]},bg=default]"
+BG_BAR="#[fg=${THEME[surface1]},bg=default]"
+TIME_COLOR="#[fg=${THEME[crust]},bg=default]"
 
 if [[ $1 =~ ^[[:digit:]]+$ ]]; then
   MAX_TITLE_WIDTH=$1
